2b41fd714fc331bc7fd13ea862695c2e20ddc198
Yu <yu@hiya.moe>
2024-09-24 20:03:28 -0600

    First commit

---
+++ CalculatorApp/Calculator.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace CalculatorApp
+{
+    public class Calculator
+    {
+        public string Expression { get; private set; }
+        public string Result { get; private set; }
+
+        public Calculator(string expression)
+        {
+        }
+
+        private string calculate()
+        {
+        }
+
+        private void checker(string expression)
+        {
+        }
+
+        private string processBrackets(string expression)
+        {
+        }
+
+        private string multiplyAndDivide(string expression)
+        {
+        }
+
+        private string addAndSubtract(string expression)
+        {
+        }
+    }
+}
---
+++ CalculatorApp/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace CalculatorApp
+{
+    public class Program
+    {
+        public static string ProcessCommand(string input)
+        {
+            try
+            {
+                return new Calculator(input).Result;
+            }
+            catch (Exception e)
+            {
+                return "Error: " + e.Message;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            string input;
+            while ((input = Console.ReadLine()) != "exit")
+            {
+                Console.WriteLine(ProcessCommand(input));
+            }
+        }
+    }
+}
